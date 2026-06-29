6753481837f6e59cc236060548a4e71a54f21c22
Tanmay07 <adhikarytanmaya@gmail.com>
2026-06-29 13:03:37 +0530

    chore: add workspace root to sys.path in packages conftest to resolve IDE import errors

--- packages/conftest.py
+++ packages/conftest.py
@@ -1,3 +1,11 @@
+import os
+import sys
+
+# Add the workspace root to sys.path so 'src' can be imported by the IDE
+workspace_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+if workspace_root not in sys.path:
+    sys.path.insert(0, workspace_root)
+
 import pytest
 from src.main import app
 from src.presentation.api import get_current_user_id
